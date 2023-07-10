8512f950304d965c3e90f00fe4109038ed7989e0
Mathitz <matheusalves789@gmail.com>
2023-07-10 00:23:44 -0300

    Writing/Reading files with PHP

---
+++ php/modulo 2/menus/api.php
@@ -0,0 +1,23 @@
+<div class="modulo azul">
+  <h3>13. API</h3>
+  <ul>
+    <li>
+      <a href="exercicio.php?dir=api&file=datas_01">
+        Datas #01
+      </a>
+    </li>
+    <li>
+      <a href="exercicio.php?dir=api&file=datas_02">
+        Datas #02
+      </a>
+    </li><li>
+      <a href="exercicio.php?dir=api&file=escrever_arquivo">
+        Escrever arquivo
+      </a>
+    </li><li>
+      <a href="exercicio.php?dir=api&file=ler_arquivo">
+        Ler arquivo
+      </a>
+    </li>
+  </ul>
+</div>
---
+++ php/modulo 2/menus/namespaces.php
@@ -0,0 +1,20 @@
+<div class="modulo laranja-escuro">
+  <h3>10. Namespace</h3>
+  <ul>
+    <li>
+      <a href="exercicio.php?dir=namespace&file=basico">
+        Exemplo básico
+      </a>
+    </li>
+    <li>
+      <a href="exercicio.php?dir=namespace&file=sub_namespaces">
+        Sub-Namespaces
+      </a>
+    </li>
+    <li>
+      <a href="exercicio.php?dir=namespace&file=use_as">
+        Use/As
+      </a>
+    </li>
+  </ul>
+</div>
